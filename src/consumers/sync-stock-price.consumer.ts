2baff8c9e8a1f7c1ed9e24da4264fd270e10b6ba
HugoHenry701 <quanghieusieubeo@gmail.com>
2023-01-18 17:35:08 +0700

    btap

--- src/consumers/sync-stock-price.consumer.ts
+++ src/consumers/sync-stock-price.consumer.ts
@@ -1,29 +1,87 @@
-import moment from "moment";
-import { codesePool, query } from "../configs/database.config";
-import { KAFKA_TOPIC } from "../constants/kafka.constant";
-import { IConsumer } from "../interfaces/IConsumer.interface";
+import moment from 'moment';
+import { codesePool, query } from '../configs/database.config';
+import { KAFKA_TOPIC } from '../constants/kafka.constant';
+import { IConsumer } from '../interfaces/IConsumer.interface';
 
 const processor = async ({ topic, partition, message }) => {
-    try {
-        const startProcess = moment()
+  try {
+    const startProcess = moment();
 
-        const data = JSON.parse(message.value.toString())
-        const { code, exchange, tradingDate, askPrice1, askVol1 } = data
+    const data = JSON.parse(message.value.toString());
 
-        const sql = `insert into StockPrice (code,exchange,trading_date,askPrice1,askVol1) values (?,?,?,?,?) on duplicate key update exchange=?, trading_date=?, askPrice1=?, askVol1=?`
-        await query(codesePool, sql, [code, exchange, tradingDate, askPrice1, askVol1, exchange, tradingDate, askPrice1, askVol1])
+    const {
+      code,
+      exchange,
+      tradingDate,
+      askPrice1,
+      askPrice2,
+      askPrice3,
+      askVol1,
+      askVol2,
+      askVol3,
+      bidPrice1,
+      bidPrice2,
+      bidPrice3,
+      bidVol1,
+      bidVol2,
+      bidVol3,
+      lastPrice,
+      totalVol,
+      refPrice,
+    } = data;
 
-        const endProcess = moment()
-        console.info(`process_time: ${endProcess.diff(startProcess, 'milliseconds')}ms`)
-    } catch (error) {
-        console.error(error)
-    }
-}
+    const sql = `insert into StockPrice (code, exchange, tradingDate, askPrice1, askPrice2, askPrice3, askVol1, askVol2, askVol3, bidPrice1, bidPrice2, bidPrice3, bidVol1, bidVol2, bidVol3, lastPrice, totalVol, refPrice) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) on duplicate key update exchange=?, tradingDate=?, askPrice1=?, askPrice2=?, askPrice3=?, askVol1=?, askVol2=?, askVol3=?, bidPrice1=?, bidPrice2=?, bidPrice3=?, bidVol1=?, bidVol2=?, bidVol3=?, lastPrice=?, totalVol=?, refPrice=?`;
+    await query(codesePool, sql, [
+      code,
+      exchange,
+      tradingDate,
+      askPrice1,
+      askPrice2,
+      askPrice3,
+      askVol1,
+      askVol2,
+      askVol3,
+      bidPrice1,
+      bidPrice2,
+      bidPrice3,
+      bidVol1,
+      bidVol2,
+      bidVol3,
+      lastPrice,
+      totalVol,
+      refPrice,
+      exchange,
+      tradingDate,
+      askPrice1,
+      askPrice2,
+      askPrice3,
+      askVol1,
+      askVol2,
+      askVol3,
+      bidPrice1,
+      bidPrice2,
+      bidPrice3,
+      bidVol1,
+      bidVol2,
+      bidVol3,
+      lastPrice,
+      totalVol,
+      refPrice,
+    ]);
+
+    const endProcess = moment();
+    console.info(
+      `process_time: ${endProcess.diff(startProcess, 'milliseconds')}ms`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const SyncStockPriceConsumer: IConsumer = {
-    name: 'sync-stock-price',
-    fromBeginning: false,
-    topicSubscribe: KAFKA_TOPIC.CODESE,
-    groupId: 'operation-group:sync-stock-price',
-    processor
-}
+  name: 'sync-stock-price',
+  fromBeginning: false,
+  topicSubscribe: KAFKA_TOPIC.CODESE,
+  groupId: 'operation-group:sync-stock-price',
+  processor,
+};
